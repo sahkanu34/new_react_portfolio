70c9a48455e60368db50967f9649e36494e45626
Suraj Sah Kanu <whoisyoursuraj@gmail.com>
2026-03-19 09:55:44 +0545

    Comment out programming languages in Skills.tsx
    Comment out C, Java, and C# skills in Skills component.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -32,9 +32,9 @@ const Skills: React.FC = () => {
       icon: <Code size={24} className="text-purple-500" />,
       skills: [
         { name: "Python", icon: <Code size={16} className="text-purple-500" />, image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
-        { name: "C", icon: <Code size={16} className="text-purple-500" />, image: "https://icon.icepanel.io/Technology/svg/C.svg" },
-        { name: "Java", icon: <Code size={16} className="text-purple-500" />, image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
-        { name: "C#", icon: <Code size={16} className="text-purple-500" />, image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" },
+        // { name: "C", icon: <Code size={16} className="text-purple-500" />, image: "https://icon.icepanel.io/Technology/svg/C.svg" },
+        // { name: "Java", icon: <Code size={16} className="text-purple-500" />, image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
+        // { name: "C#", icon: <Code size={16} className="text-purple-500" />, image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" },
         // { name: "JavaScript", icon: <Code size={16} className="text-purple-500" /> },
       ]
     },
